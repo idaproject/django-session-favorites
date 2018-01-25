3ef38e9af9c8cb82ea95dfa74748221ea9012d7a
Alexander Temnikov <alexander.temnikov@me.com>
2018-01-25 14:13:59 +0300

    Add app_name to urlconf for Django 2.0 support

--- session_favorites/urls.py
+++ session_favorites/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from .views import FavoriteAddView, FavoriteRemoveView
 
+app_name = 'session_favorites'
 urlpatterns = [
     url(r'^(?P<pk>[\d]+)/add/$', view=FavoriteAddView.as_view(), name='add'),
     url(r'^(?P<pk>[\d]+)/remove/$', view=FavoriteRemoveView.as_view(), name='remove'),
